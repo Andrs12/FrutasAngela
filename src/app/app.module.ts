7130090bb9c9756e28de81f2cabda884f04a7499
Andres Gomez Alvarez <andrsgomez12@gmail.com>
2021-05-05 23:21:02 +0200

    05/05/2021
    05/05/2021 - NAVBAR, SIDEBAR, INDEX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
